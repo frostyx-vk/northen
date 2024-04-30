9ee6a53a090a54f1dd44491f2f5951908f5f1aa9
frostyx-vk <vk.xam@yandex.ru>
2024-05-01 00:51:57 +0700

    work with homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './HomePage.css'
+import { ReactComponent as Arrow } from './../../svg/arrow-r.svg'
+import { dataWebSolutions, dataTabSolutions } from './../../components/data'
 
 export default function HomePage() {
+    const [content, setContent] = useState('dev');
+
+    function handleClick(type) {
+        setContent(type);
+    };
+
     return (
         <main className="content">
-            HomePage
+            <div className="wrapper">
+                <div className="content-solution__right-block">
+                    <div className="content-solution__web-block">
+                        <h2>Web-решения<br />для вашего бизнеса</h2>
+                        <ul>
+                            {
+                                dataWebSolutions.map((item, i) => {
+                                    return <li key={i}>
+                                        <a className="content-solution__web-link" id={i}>
+                                            {item.title}
+                                            <Arrow />
+                                        </a>
+                                    </li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div className="content-solution__develop-block">
+                        <div style={{height: '250px'}}>
+                            <div className="content-solution__develop-tabs">
+                                <button className={content === 'dev' ? 'is-active' : null} onClick={() => handleClick('dev')}>
+                                    Разработка
+                                </button>
+                                <button className={content === 'sup' ? 'is-active' : null} onClick={() => handleClick('sup')}>
+                                    Поддержка
+                                </button>
+                                <button className={content === 'ret' ? 'is-active' : null} onClick={() => handleClick('ret')}>
+                                    Ретейнер
+                                </button>
+                            </div>
+                            <div className="content-solution__develop-text">
+                                <p>{dataTabSolutions[content]}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
