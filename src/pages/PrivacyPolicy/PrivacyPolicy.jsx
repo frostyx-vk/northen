f7ea542c3314632e35d6cfbf06f57903d262900f
frostyx-vk <vk.xam@yandex.ru>
2024-04-30 12:18:13 +0700

    fill privacy

--- src/pages/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './PrivacyPolicy.css'
+import { dataPrivacy } from './../../components/data'
 
 export default function PrivacyPolicy() {
     return (
         <main className="content">
-            Privacy Politic
+            <div className='wrapper'>
+                <div className="flatpage-header">
+                    <h1>{dataPrivacy[0].title}</h1>
+                </div>
+                <div className='flatpage-content'>
+                    {dataPrivacy[0].description}
+                </div>
+            </div>
         </main>
     )
 }
