d407d9b62d87aaa1b4b238a9a7c40f043e89b42f
Vladimir_Filippov <v.filippov1994@gmail.com>
2024-09-02 12:10:32 +0700

    Доработка раздела "Контакты"

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {
     ADVANTAGE_LIST,
+    CONTACT_LIST,
     SERVICE_LIST
 } from '../api/urls'
 
@@ -8,5 +9,6 @@ let serverUrl = 'http://localhost:8000';
 
 export let serverBaseUrl = `${serverUrl}/api/`;
 
-export const serviceList = await axios.get(`${serverBaseUrl}${SERVICE_LIST}`).then(res => res.data);
 export const advantageList = await axios.get(`${serverBaseUrl}${ADVANTAGE_LIST}`).then(res => res.data);
+export const serviceList = await axios.get(`${serverBaseUrl}${SERVICE_LIST}`).then(res => res.data);
+export const contactList = await axios.get(`${serverBaseUrl}${CONTACT_LIST}`).then(res => res.data);
--- src/api/urls.js
+++ src/api/urls.js
@@ -1,2 +1,3 @@
 export const SERVICE_LIST = `services/`;
 export const ADVANTAGE_LIST = `advantages/`;
+export const CONTACT_LIST = `contacts/`;
