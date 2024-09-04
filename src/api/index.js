fec597209587ed2c14704d79ab2ff491f4c25b04
Vladimir_Filippov <v.filippov1994@gmail.com>
2024-09-04 11:25:34 +0700

    Вывод слайдера из бд

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ import {
     CONTACT_LIST,
     CREATE_ORDER_ONLINE,
     CREATE_ORDER_PHONE,
+    PARTNERS_LIST,
     SERVICE_LIST,
     SPECIALIZATIONS_LIST,
     TECHNOLOGY_LIST
@@ -18,6 +19,7 @@ export const serviceList = await axios.get(`${serverBaseUrl}${SERVICE_LIST}`).th
 export const contactList = await axios.get(`${serverBaseUrl}${CONTACT_LIST}`).then(res => res.data);
 export const specilaizationList = await axios.get(`${serverBaseUrl}${SPECIALIZATIONS_LIST}`).then(res => res.data);
 export const technologyList = await axios.get(`${serverBaseUrl}${TECHNOLOGY_LIST}`).then(res => res.data);
+export const partnerList = await axios.get(`${serverBaseUrl}${PARTNERS_LIST}`).then(res => res.data);
 export function createOrderPhone(userData) {
     return axios.post(`${serverBaseUrl}${CREATE_ORDER_PHONE}`, userData)
 }
--- src/api/urls.js
+++ src/api/urls.js
@@ -4,4 +4,5 @@ export const CONTACT_LIST = `contacts/`;
 export const SPECIALIZATIONS_LIST = `specializations/`;
 export const TECHNOLOGY_LIST = `technologys/`;
 export const CREATE_ORDER_PHONE = `create-order-phone/`;
-export const CREATE_ORDER_ONLINE = `create-order-online/`
+export const CREATE_ORDER_ONLINE = `create-order-online/`;
+export const PARTNERS_LIST = 'partners/';
